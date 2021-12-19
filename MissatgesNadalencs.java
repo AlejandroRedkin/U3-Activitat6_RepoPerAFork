e0e2ce9ae999c758e005fefbd194e6ad01c64134
Alejandro Redkin Zemlyanukhin <aleredzem@gmail.com>
2021-12-19 14:49:36 +0100

    modificacio AlejandroRedkin

---
+++ MissatgesNadalencs.java
@@ -0,0 +1,8 @@
+private static void AlumneFila3PC3() {
+		
+		System.out.println("Hola, sóc Alejandro Redkin");
+		System.out.println(" BON NADAL, Salut a tots.");
+		System.out.println(" FELIÇ ANY NOU !!!!!!");
+		System.out.println();
+
+	}
--- src/ed/empar/MissatgesNadalencs.java
+++ src/ed/empar/MissatgesNadalencs.java
@@ -162,6 +162,11 @@ public class MissatgesNadalencs {
 
 	private static void AlumneFila3PC3() {
 
+		System.out.println("Hola, sóc Alejandro Redkin");
+		System.out.println(" BON NADAL, Salut a tots.");
+		System.out.println(" FELIÇ ANY NOU !!!!!!");
+		System.out.println();
+
 	}
 
 	private static void AlumneFila3PC4() {
